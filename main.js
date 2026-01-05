78e0accac0ede72ebc876b88c2f98e508e9afa3c
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-01-05 14:24:16 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const productsDiv = document.querySelector('.productArea')
+
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(products => {
+       products.forEach(product => {
+         const div  = document.createElement('div');
+
+         div.innerHTML = `
+          <img class = 'images' src='${product.image}'>
+          <h3 class = 'hh3'>${product.title}</h3>
+          <p> Rate: ${product.rating.rate} / 10</p>
+          <p class='price'>${product.price} $</p>
+         `;
+       
+      productsDiv.appendChild(div)
+       })
+  })
